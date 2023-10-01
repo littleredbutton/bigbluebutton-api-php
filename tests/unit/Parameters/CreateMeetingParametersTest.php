20ca055c8da7c9e77e3a52f3f962703eeb69b0fc
Felix Jacobi <Felix@jacobi-hamburg.net>
2023-10-01 14:37:01 +0200

    Updated PHPUnit to version 9 (fixes #89)

--- tests/unit/Parameters/CreateMeetingParametersTest.php
+++ tests/unit/Parameters/CreateMeetingParametersTest.php
@@ -21,7 +21,6 @@ namespace BigBlueButton\Parameters;
 
 use BigBlueButton\Core\GuestPolicy;
 use BigBlueButton\TestCase;
-use PHPUnit\Framework\Error\Warning;
 
 /**
  * Class CreateMeetingParametersTest.
@@ -123,7 +122,7 @@ class CreateMeetingParametersTest extends TestCase
 
     public function testCreateBreakoutMeetingWithMissingParams()
     {
-        $this->expectException(Warning::class);
+        $this->expectException(\RuntimeException::class);
 
         $params = new CreateMeetingParameters($this->faker->uuid, $this->faker->name);
         $params->setBreakout(true);
