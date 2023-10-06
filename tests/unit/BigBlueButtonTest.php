ed4f2b0159e8648b3ac30df1db02b1d88d52eabe
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2023-10-06 13:49:49 +0200

    Fix test

--- tests/unit/BigBlueButtonTest.php
+++ tests/unit/BigBlueButtonTest.php
@@ -353,7 +353,7 @@ class BigBlueButtonTest extends TestCase
     public function testBuildUrl(): void
     {
         // Test with default hash algorithm (sha1)
-        $bigBlueButton = new BigBlueButton('https://bbb.example/bigbluebutton/', 'S3cr3t', hashingAlgorithm: HashingAlgorithm::SHA_1);
+        $bigBlueButton = new BigBlueButton('https://bbb.example/bigbluebutton/', 'S3cr3t', null, HashingAlgorithm::SHA_1);
 
         $this->assertSame(
             'https://bbb.example/bigbluebutton/api/foo?foo=bar&baz=bazinga&checksum=694ad46bc5a79a572bab6c8b9a939527c39ac7f6',
@@ -362,7 +362,7 @@ class BigBlueButtonTest extends TestCase
         );
 
         // Test with different hash algorithm (sha256)
-        $bigBlueButton = new BigBlueButton('https://bbb.example/bigbluebutton/', 'S3cr3t', hashingAlgorithm: HashingAlgorithm::SHA_256);
+        $bigBlueButton = new BigBlueButton('https://bbb.example/bigbluebutton/', 'S3cr3t', null, HashingAlgorithm::SHA_256);
 
         $this->assertSame(
             'https://bbb.example/bigbluebutton/api/foo?foo=bar&baz=bazinga&checksum=0ce0d779a8220be9824c7eab055b36b59ac504ba899a76d7c528b8473960025e',
