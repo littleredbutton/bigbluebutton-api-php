f6efc6daacb62ad48df64f5103606857c4488dc8
Felix Jacobi <felix.jacobi@iserv.eu>
2024-03-15 02:35:25 +0100

    Updated rector configs

--- rector.php
+++ rector.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
@@ -16,6 +19,9 @@ return RectorConfig::configure()
     ->withPhpSets()
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
+        RemoveUnusedVariableAssignRector::class,
+        RemoveUselessParamTagRector::class,
+        RemoveUselessReturnTagRector::class,
         TypedPropertyFromAssignsRector::class,
         TypedPropertyFromStrictConstructorRector::class,
         TypedPropertyFromStrictSetUpRector::class,
