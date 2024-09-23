85024b8e9f5f02313ce458e073e47e325e1088c8
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-09-23 09:33:54 +0000

    Add infiniteWhiteboard to features enum

--- src/Enum/Feature.php
+++ src/Enum/Feature.php
@@ -48,4 +48,5 @@ enum Feature: string
     case DOWNLOAD_PRESENTATION_ORIGINAL_FILE = 'downloadPresentationOriginalFile';
     case DOWNLOAD_PRESENTATION_CONVERTED_TO_PDF = 'downloadPresentationConvertedToPdf';
     case TIMER = 'timer';
+    case INFINITE_WHITEBOARD = 'infiniteWhiteboard';
 }
