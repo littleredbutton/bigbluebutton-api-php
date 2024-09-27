0df16da657a8555d7f880edf3e797e5ae0ade5dc
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-09-28 00:04:05 +0200

    Add learningDashboardDownloadSessionData to Feature enum (#213)

--- src/Enum/Feature.php
+++ src/Enum/Feature.php
@@ -36,6 +36,7 @@ enum Feature: string
     case IMPORT_SHARED_NOTES_FROM_BREAKOUT_ROOMS = 'importSharedNotesFromBreakoutRooms';
     case LAYOUTS = 'layouts';
     case LEARNING_DASHBOARD = 'learningDashboard';
+    case LEARNING_DASHBOARD_DOWNLOAD_SESSION_DATA = 'learningDashboardDownloadSessionData';
     case POLLS = 'polls';
     case SCREENSHARE = 'screenshare';
     case SHARED_NOTES = 'sharedNotes';
